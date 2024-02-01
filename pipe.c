df96c3e7a03339b418280e29af1434ced31c03ec
ugathoo <urjagathoo@ucla.edu>
2024-02-01 11:43:41 -0800

    added close

--- pipe.c
+++ pipe.c
@@ -87,6 +87,7 @@ int main(int argc, char * argv[]) {
 				printf("%s\n",argv[i+1]);
 				int status = 0;
 				int pid = ret;
+				close(pipefd[0]);
 				waitpid(pid, &status, 0);
 				printf("%d\n", WEXITSTATUS(status));
 			}
