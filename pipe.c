b9676744507a5a55a6961023cc548cd13f1eb627
ugathoo <urjagathoo@ucla.edu>
2024-02-01 12:42:18 -0800

    typo

--- pipe.c
+++ pipe.c
@@ -102,7 +102,7 @@ int main(int argc, char * argv[]) {
 			int status = 0;
 			int pid = ret;
 			waitpid(pid, &status, 0);
-			printf("%d\n", WEXITSTATUS(status));*/
+			printf("%d\n", WEXITSTATUS(status));
 			
 		}
  	}
