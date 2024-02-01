b15197ff1fed993ec1c3b5330c9418c617a6bd1c
ugathoo <urjagathoo@ucla.edu>
2024-01-31 23:43:22 -0800

    move wait

--- pipe.c
+++ pipe.c
@@ -97,11 +97,13 @@ int main(int argc, char * argv[]) {
 					}
 				} 
 				printf("parent process only 2 args\n");
-				int status = 0;
-				wait(ret2, &status, 0);
-				printf("%d\n"WEXITSTATUS(status));
+				
 				
 			}
+			int status = 0;
+			int pid = ret;
+			wait(pid, &status, 0);
+			printf("%d\n"WEXITSTATUS(status));
  		}
  	}
 } 
