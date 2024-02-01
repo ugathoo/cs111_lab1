7210932990c5ca30601cbaef216977ee91943199
ugathoo <urjagathoo@ucla.edu>
2024-01-31 16:11:37 -0800

    two args accounted for

--- pipe.c
+++ pipe.c
@@ -22,6 +22,46 @@ int main(int argc, char *argv[])
 			return errno;
 		}
 	}
+	//exactly two programs edge case
+	else if(argc == 3){
+		int pipefd[2];
+		if(pipe(pipefd) == -1){
+			return errno;
+		}
+		int ret = fork();
+		if(ret < 0){
+			return errno;
+		}
+		else if (ret == 0){
+			printf("Child process\n");
+			dup2(pipefd[1], STDOUT_FILENO);
+			close(pipefd[0]);
+			close(pipefd[1]);
+			//check valid arg
+			if(execlp(argv[1], argv[1], NULL) == -1){
+				return errno;
+			}
+			else{
+				printf("parent process\n");
+				close(pipefd[1]);
+				int ret2 = fork();
+				if (ret2 < 0){
+					return errno;
+				}
+				else if (ret2 == 0){
+					printf("parent child process\n");
+					dup2(pipefd[0], STDIN_FILENO);
+					close(pipefd[0]);
+					if(execlp(argv[2], argv[2], NULL) == -1){
+						return errno;
+					}
+				}
+				else{
+					close(pipefd[0]);
+				}
+			}
+		}
+	}
 	
 
 	// TODO: it's all yours
