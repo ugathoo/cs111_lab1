210dd712fe76cc735cebbe39c1738b3f9be6900b
ugathoo <urjagathoo@ucla.edu>
2024-02-01 13:34:26 -0800

    o

--- pipe.c
+++ pipe.c
@@ -64,34 +64,37 @@ int main(int argc, char * argv[]) {
  		int pipefd[2];
  		int pipectr = argc - 2;
  		int ret = 0;
-		if (pipe(pipefd) == -1) {
+		
+		printf("before loop\n");
+ 		for (int i = 0; i < pipectr; i+=1) {
+			printf("temp\n");
+			if (pipe(pipefd) == -1) {
 				printf("error pipe\n");
 				return errno;
-		}
-		printf("before loop");
- 		for (int i = 0; i < pipectr; i+=1) {
-			printf("temp");
-			
+			}
 			ret = fork();
+			printf("forked\n");
 			if (ret < 0) {
 				printf("error forking\n");
 				return errno;
 			} else if (ret == 0) {
 				dup2(pipefd[1], STDOUT_FILENO); //write to pipefd[1], read from STDOUT_FILENO
+				printf("duped\n");
 				int id = i+1;
 				if (execlp(argv[id], argv[id], NULL) == -1) {
 					printf("2 args error\n");
 					return errno;
 				}
+				printf("exited\n");
 			} else {
 				int status = 0;
 				int pid = ret;
 				waitpid(pid, &status, 0);
-				printf("before wait");
+				printf("cleared wait\n");
 				printf("%d\n", WEXITSTATUS(status));
-				printf("clear status");
+				printf("cleared status print\n");
 				dup2(pipefd[0], STDIN_FILENO);
-				printf("hit");
+				printf("hit after dup2\n");
 				//close(pipefd[0]);
 			}
  		}
