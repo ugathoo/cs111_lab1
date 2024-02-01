17dd811f48254f7a99c5d5eca8bd785acfc6292d
ugathoo <urjagathoo@ucla.edu>
2024-01-31 23:36:16 -0800

    added wait

--- pipe.c
+++ pipe.c
@@ -8,41 +8,41 @@
 
 int main(int argc, char * argv[]) {
  	if (argc == 1) {
- 		printf("invalid number of args\n");
+ 		//printf("invalid number of args\n");
  		exit(EINVAL);
 	//only 1 arg
  	} else if (argc == 2) {
  		if (execlp(argv[1], argv[1], NULL) == -1) {
- 			printf("invalid command for one arg\n");
+ 			//printf("invalid command for one arg\n");
  			return errno;
  		}
 	//only 2 args
  	} else if (argc == 3) {
  		int pipefd[2];
  		if (pipe(pipefd) == -1) {
- 			printf("error pipe\n");
+ 			//printf("error pipe\n");
 			return errno;
  		}
  		int ret = fork();
  		if (ret < 0) {
- 			printf("error forking\n");
+ 			//printf("error forking\n");
  			return errno;
  		} else if (ret == 0) {
  			dup2(pipefd[1], STDOUT_FILENO); 
  			close(pipefd[0]);
  			close(pipefd[1]);
 			if (execlp(argv[1], argv[1], NULL) == -1) {
-				printf("2 args error\n");
+				//printf("2 args error\n");
 				return errno;
 			}
  		} else {
- 			printf("parent process only 2 args\n");
+ 			//printf("parent process only 2 args\n");
  			close(pipefd[1]);
  			int ret2 = fork();
  			if (ret2 == -1)
- 				printf("error forking");
+ 				//printf("error forking");
  			else if (ret2 == 0) {
- 				printf("parent child process only 2 args\n");
+ 				//printf("parent child process only 2 args\n");
  				dup2(pipefd[0], STDIN_FILENO);
  				close(pipefd[0]);
 				if (execlp(argv[2], argv[2], NULL) == -1) {
@@ -50,7 +50,7 @@ int main(int argc, char * argv[]) {
 					return errno;
 				}
 			} else {
- 				printf("parent process only 2 args\n");
+ 				//printf("parent process only 2 args\n");
  				close(pipefd[0]);
  			}
  		}
@@ -97,6 +97,7 @@ int main(int argc, char * argv[]) {
 				} else {
 					printf("parent process only 2 args\n");
 					close(pipefd[0]);
+					wait(NULL);
 				}
 			}
  		}
