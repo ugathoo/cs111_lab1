24722bebc30b5bd4afe4a96936ff12494b3cd0f3
ugathoo <urjagathoo@ucla.edu>
2024-02-02 12:51:24 -0800

    ggg

--- pipe.c
+++ pipe.c
@@ -76,7 +76,6 @@ int main(int argc, char * argv[]) {
 				perror("fork error");
 				return errno;
 			} else if (ret == 0) {
-				dup2(pipefd[0], STDIN_FILENO);
 				//close(pipefd[0]); 
 				dup2(pipefd[1], STDOUT_FILENO);
 				//close(pipefd[1]); 
@@ -86,8 +85,6 @@ int main(int argc, char * argv[]) {
 					exit(errno);
 				}
 			} else {
-				
-
 				//close(pipefd[1]); 
 			}
 		}
