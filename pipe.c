d5844b5cc54d9128801b74b6639268a287d09008
ugathoo <urjagathoo@ucla.edu>
2024-02-02 12:20:52 -0800

    oo

--- pipe.c
+++ pipe.c
@@ -112,6 +112,7 @@ int main(int argc, char * argv[]) {
 			return errno;
 		} else if (ret == 0) {
 			dup2(pipefd[0], STDIN_FILENO);
+			dup2(pipefd[1], STDOUT_FILENO);
 			close(pipefd[0]);
 			close(pipefd[1]);
 			//printf("duped last arg\n");
